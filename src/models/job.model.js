92553907e5ebe08960bd0320ad2672174c2dfa05
tshimsthedoor <tshims79@gmail.com>
2026-06-07 20:24:34 +0200

    implement job model CRUD operations

--- src/models/job.model.js
+++ src/models/job.model.js
@@ -21,6 +21,52 @@ async function getJobsByUserId(userId, filters = {}) {
 
 // Get a job by id
 async function getJobById(jobId, userId){
-    
+    const result = await pool.query('SELECT * FROM jobs WHERE id = $1 AND user_id = $2', [jobId, userId]);
+    return result.rows[0];
 }
+
+// create a new job
+async function createJob(jobData, userId){
+    const {title, company, location, job_url, status, notes, applied_date} = jobData;
+    const result = await pool.query(`INSERT INTO jobs (user_id, title, company, location, job_url, status, notes, applied_date) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id, title, company, location, job_url, status, notes, applied_date`, [userId, title, company, location, job_url, status, notes, applied_date]);
+    return result.rows[0];
+}
+
+// Update a job
+async function updateJob(jobId, userId, jobData) {
+    const allowedFields = ['title', 'company', 'location', 'job_url', 'status', 'notes', 'applied_date'];
+    const setClause = [];
+    const values = [];
+    let paramIndex = 1;
+
+    // Build set clause and values array
+    for (const field of allowedFields) {
+        if (jobData[field] !== undefined) {
+            setClause.push(`${field} = $${paramIndex}`);
+            values.push(jobData[field]);
+            paramIndex++;
+        }
+    }
+    if(setClause.length === 0) return null;
+
+    values.push(jobId, userId);
+    const query = `UPDATE jobs SET ${setClause.join(', ')}, updated_at = CURRENT_TIMESTAMP WHERE id = $${paramIndex} AND user_id = $${paramIndex + 1} RETURNING id, title, company, location, job_url, status, notes, applied_date`;
+
+    const result = await pool.query(query, values);
+    return result.rows[0];
+}
+
+// Delete a job
+async function deleteJob(jobId, userId) {
+    const result = await pool.query('DELETE FROM jobs WHERE id = $1 AND user_id = $2 RETURNING id', [jobId, userId]);
+    return result.rows[0];
+}
+
+module.exports = {
+    getJobsByUserId,
+    getJobById,
+    createJob,
+    updateJob,
+    deleteJob,
+};
     
